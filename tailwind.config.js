0a9097c61411aad807e96bfde67745dc050abfdb
OguikeGodswill <108469994+dammybundlez@users.noreply.github.com>
2025-06-16 02:43:17 -0700

    Update tailwind.config.js
    typo error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./app/components/widgets/CalendarWidget.tsx' , './app/components/widgets/ClockWidget.tsx' , './app/components/widgets/SpotifyWidget.tsx' , './app/components/widgets/NewsWidget.tsx' , './app/components/widgets/CryptoWidget.tsx' ,
-    './app/components/widgets/NoteWidget.tsx' , './app/components/widgets/QuoteWidget.tsx' , './app/components/widgets/TOdoWidget.tsx' , './app/components/widgets/WeatherWidget.tsx' , './app/components/layout/MainLayout.tsx' , './app/components/WidgetContainer.tsx',
+    './app/components/widgets/NoteWidget.tsx' , './app/components/widgets/QuoteWidget.tsx' , './app/components/widgets/TodoWidget.tsx' , './app/components/widgets/WeatherWidget.tsx' , './app/components/layout/MainLayout.tsx' , './app/components/WidgetContainer.tsx',
     './app/components/ThemeToggle.tsx' , './app/page.tsx'
    ],
   theme: {
